97f68a5fc914000f2c4a91fc53fdc65769081739
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-22 21:54:17 +0300

    mola

--- Alican/Models/MyAuthorize.cs
+++ Alican/Models/MyAuthorize.cs
@@ -5,13 +5,11 @@ namespace Alican.Models
 {
     public sealed class MyAuthorize :Attribute, IAuthorizationFilter
     {
-        public async void OnAuthorization(AuthorizationFilterContext context)
+        public  void OnAuthorization(AuthorizationFilterContext context)
         {
             string? secretKey = context.HttpContext.Request.Headers.FirstOrDefault(p => p.Key == "SecretKey").Value;
             if(string.IsNullOrEmpty(secretKey) && secretKey !="My Secret")
             {
-                context.HttpContext.Response.StatusCode = 401;
-                await context.HttpContext.Response.WriteAsync("giriş yapma yetkiniz yok");
                 context.Result=new UnauthorizedResult();
 
             }
