e37f278d81e8d5ff5e1e6d9a8123957facfcafa6
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-22 20:39:22 +0300

    ..

---
+++ Alican/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alican.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new {Message="Api is working"});
+        }
+    }
+}
