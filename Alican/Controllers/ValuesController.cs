2e8986c2c827cbe4e319061232169519fe07a58d
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-05-22 21:28:11 +0300

    route params

--- Alican/Controllers/ValuesController.cs
+++ Alican/Controllers/ValuesController.cs
@@ -3,16 +3,22 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Alican.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Test()
+        [HttpGet("{age}")] // route params
+        public IActionResult Test(string age)
         {
             var context = HttpContext;
-            throw new ArgumentException("hata");
+
             return Ok(new {Message="Api is working"});
+        }
+        [HttpPost]
+        public IActionResult Create(ProductDto productDto)
+        {
+            return Ok();
         }
     }
 }
+public sealed record ProductDto(int Id,string Name);
